d16b9ff077f003e6bd888706811f6d4223b8a9f0
Alec Aoki <alec.camposaoki@gmail.com>
2024-05-23 09:48:13 -0300

    End of class

--- Aula10/aloc_dinam.c
+++ Aula10/aloc_dinam.c
@@ -87,6 +87,44 @@ ALOCAÇÃO DINÂMICA (doideira)
     Imprima_Valores
 */
 
+/* ALOCAÇÃO DINÂMICA DE MATRIZES
+-> alocar vetor de ponteiros que aí sim apontam para onde os valores estão
+ex:
+double **aloca(int linha, int coluna){
+    double **p;
+    //alocação do vetor de ponteiros:
+    p = (double **)malloc(linha*sizeof(double *));
+    //alocação do espaço para os valores
+    for (int i=0; i<linha; linha++){
+        p[i] = (double *)malloc(coluna*sizeof(double));
+    }
+}
+return p;
+
+obs: para usar a matriz, basta fazer p[i][j]
+obs: alocação de matriz de string tem 3 dimensões, sendo uma 2x2 contendo um vetor dentro
+
+ALOCAÇÃO DINÂMICA DE MATRIZES v2
+-> alocar o vetor de ponteiros e daí alocar um vetor só para os valores, dividindo ele no número de colunas
+-> aí chamamos o malloc só duas vezes, em vez da várias vezes dentro de um for
+ex:
+double **aloca(int linha, int coluna){
+    double **p;
+    p = (double **)malloc(linha*sizeof(double *));
+    double *v;
+    v = (double *)malloc((linha*coluna)*sizeof(double));
+    for (int i=0; i<linha; i++){
+        p[i] = &v[i*coluna]; // <=> p[i] = v + (i*coluna);  (aritmética de ponteiros)
+    }
+
+    return p;
+}
+*/
+
+/* DESALOCAÇÃO
+-> temos que desalocar o ponteiro que aponta para a matriz e o ponteiro que aponta para o vetor de ponteiros 
+*/
+
 double *Aloque(int tam){
     //como vamos retornar um endereço (ponteiro), temos que declarar a função como um ponteiro
     double *vetor_Aloque;
