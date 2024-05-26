8e3cc62fcb515f93147d5d8a053c1582cbc80901
Alec Aoki <alec.camposaoki@gmail.com>
2024-05-26 17:39:30 -0300

    Logic

--- Aula10/jogo.c
+++ Aula10/jogo.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Programa:
+-> receber linhas e colunas
+-> Preencha_Matriz();
+    -> preenche a matriz de acordo com a entrada
+-> Cheque_Matriz();
+    -> define que posições podem ou não ser ocupadas
+    -> para cada '.', checar [i][j+1] (em cima), [i][j-1] (embaixo), [i-1][j] (esquerda) e [i+1][j] (direita)
+        -> se o caracter for igual a X, add. 1 a um contador
+    -> se a quant. final no contador NÃO for ímpar, trocar '.' por '*'
+-> Imprima_Matriz();
+*/
+
 int main(void){
-
-
+    
     return 0;
 }
