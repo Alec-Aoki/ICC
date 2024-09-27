86e4c89b293941f34d98a1498d5561fbab211982
Alec Aoki <alec.camposaoki@gmail.com>
2024-09-27 17:31:36 -0300

    Bubblesort working

--- LAB_ICC2/Aula05/main.c
+++ LAB_ICC2/Aula05/main.c
@@ -19,31 +19,50 @@ int main(void){
     int quantPratos = 0;
     scanf("%d", &quantPratos);
 
-    PRATO vetPratos[quantPratos];
+    PRATO *pontVetPratos = (PRATO *)malloc(quantPratos*sizeof(PRATO));
+    if(pontVetPratos == NULL) exit(1);
+
     for(int i=0; i<quantPratos; i++){
-        scanf("%d %d %s", &vetPratos[i].prioridade, &vetPratos[i].tempoPreparo, vetPratos[i].nome);
-        vetPratos[i].nome[strlen(vetPratos[i].nome)] = '\0';
+        scanf("%d %d %s", &pontVetPratos[i].prioridade, &pontVetPratos[i].tempoPreparo, pontVetPratos[i].nome);
+        pontVetPratos[i].nome[strlen(pontVetPratos[i].nome)] = '\0';
     }
 
-    //bubblesort(vetPratos, quantPratos);
+    bubblesort(pontVetPratos, quantPratos);
     //quicksort(vetPratos, 0, quantPratos-1);
 
-    imprimir_pratos(vetPratos, quantPratos);
+    imprimir_pratos(pontVetPratos, quantPratos);
+
+    free(pontVetPratos);
 
     return 0;
 }
 
-//Retorna true se prato1 > prato2 (prioridade ou tempo de preparo)
+//Retorna true se prato1 > prato2 (prioridade, tempo de preparo)
 bool comparar_pratos(PRATO prato1, PRATO prato2){
     if(prato1.prioridade > prato2.prioridade) return true;
     else if(prato1.prioridade == prato2.prioridade){
-        if(prato1.tempoPreparo > prato2.tempoPreparo) return true;
+        if(prato1.tempoPreparo < prato2.tempoPreparo) return true;
     }
     
     return false;
 }
 
-void bubblesort(PRATO vetorPratos[], int tam){
+void bubblesort(PRATO *vetPratos, int tam){
+    for(int i=0; i<tam; i++){
+        int trocas = 0;
+
+        for(int j=1; j<tam-i; j++){
+            if(comparar_pratos(vetPratos[j-1], vetPratos[j])){
+                PRATO aux = vetPratos[j-1];
+                vetPratos[j-1] = vetPratos[j];
+                vetPratos[j] = aux;
+
+                trocas++;
+            }
+        }
+
+        if(trocas == 0) return;
+    }
 
     return;
 }
@@ -53,8 +72,9 @@ void quicksort(PRATO v[], int inicio, int fim){
     return;
 }
 
-void imprimir_pratos(PRATO v[], int tam){
+void imprimir_pratos(PRATO *v, int tam){
     for(int i=0; i<tam; i++){
+        //printf("%d %d %s\n", v[i].prioridade, v[i].tempoPreparo, v[i].nome);
         printf("%s\n", v[i].nome);
     }
 
