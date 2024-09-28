3465378060039364d68cbf400d74fa0dcfa30978
Alec Aoki <alec.camposaoki@gmail.com>
2024-09-27 21:27:41 -0300

    Unfinished

--- LAB_ICC2/Aula05/main.c
+++ LAB_ICC2/Aula05/main.c
@@ -10,10 +10,10 @@ struct prato_{
     int tempoPreparo;
 };
 
-bool comparar_pratos(PRATO prato1, PRATO prato2);
-void bubblesort(PRATO v[], int tam);
-void quicksort(PRATO v[], int inicio, int fim);
-void imprimir_pratos(PRATO v[], int tam);
+bool comparar_pratos(PRATO prato1, PRATO prato2, bool quicksort);
+void bubblesort(PRATO *pontVetPratos, int tam);
+void quicksort(PRATO *pontVetPratos, int inicio, int fim);
+void imprimir_pratos(PRATO *pontVetPratos, int tam);
 
 int main(void){
     int quantPratos = 0;
@@ -27,8 +27,8 @@ int main(void){
         pontVetPratos[i].nome[strlen(pontVetPratos[i].nome)] = '\0';
     }
 
-    bubblesort(pontVetPratos, quantPratos);
-    //quicksort(vetPratos, 0, quantPratos-1);
+    //bubblesort(pontVetPratos, quantPratos);
+    quicksort(pontVetPratos, 0, quantPratos-1);
 
     imprimir_pratos(pontVetPratos, quantPratos);
 
@@ -38,24 +38,25 @@ int main(void){
 }
 
 //Retorna true se prato1 > prato2 (prioridade, tempo de preparo)
-bool comparar_pratos(PRATO prato1, PRATO prato2){
+bool comparar_pratos(PRATO prato1, PRATO prato2, bool quicksort){
     if(prato1.prioridade > prato2.prioridade) return true;
     else if(prato1.prioridade == prato2.prioridade){
-        if(prato1.tempoPreparo < prato2.tempoPreparo) return true;
+        if(quicksort) return true;
+        else if(prato1.tempoPreparo < prato2.tempoPreparo) return true;
     }
     
     return false;
 }
 
-void bubblesort(PRATO *vetPratos, int tam){
+void bubblesort(PRATO *pontVetPratos, int tam){
     for(int i=0; i<tam; i++){
         int trocas = 0;
 
         for(int j=1; j<tam-i; j++){
-            if(comparar_pratos(vetPratos[j-1], vetPratos[j])){
-                PRATO aux = vetPratos[j-1];
-                vetPratos[j-1] = vetPratos[j];
-                vetPratos[j] = aux;
+            if(comparar_pratos(pontVetPratos[j-1], pontVetPratos[j], false)){
+                PRATO aux = pontVetPratos[j-1];
+                pontVetPratos[j-1] = pontVetPratos[j];
+                pontVetPratos[j] = aux;
 
                 trocas++;
             }
@@ -67,15 +68,14 @@ void bubblesort(PRATO *vetPratos, int tam){
     return;
 }
 
-void quicksort(PRATO v[], int inicio, int fim){
-    
+void quicksort(PRATO *pontVetPratos, int inicio, int fim){
     return;
 }
 
-void imprimir_pratos(PRATO *v, int tam){
+void imprimir_pratos(PRATO *pontVetPratos, int tam){
     for(int i=0; i<tam; i++){
         //printf("%d %d %s\n", v[i].prioridade, v[i].tempoPreparo, v[i].nome);
-        printf("%s\n", v[i].nome);
+        printf("%s\n", pontVetPratos[i].nome);
     }
 
     return;
