7a75a7f06c066fd31e7b595313058bb11da7de58
Alec Aoki <alec.camposaoki@gmail.com>
2024-10-04 15:46:11 -0300

    Mergesort added

--- LAB_ICC2/Aula06/Trabalho6/main.c
+++ LAB_ICC2/Aula06/Trabalho6/main.c
@@ -10,8 +10,13 @@ struct prato_{
     int tempoPreparo;
 };
 
+/*funções base*/
 bool comparar_pratos(PRATO prato1, PRATO prato2);
 void imprimir_pratos(PRATO *pontVetPratos, int tam);
+
+/*funcões mergesort*/
+void intercala(PRATO esquerda[], int tam_esq, PRATO direita[], int tam_dir, PRATO *v);
+void mergesort(PRATO *v, int tam);
 
 int main(void){
 
@@ -26,9 +31,73 @@ int main(void){
         pontVetPratos[i].nome[strlen(pontVetPratos[i].nome)] = '\0';
     }
 
+    mergesort(pontVetPratos, quantPratos);
+
+    imprimir_pratos(pontVetPratos, quantPratos);
+
     free(pontVetPratos);
 
     return 0;
+}
+
+/*mergesort*/
+void intercala(PRATO esquerda[], int tam_esq, PRATO direita[], int tam_dir, PRATO *v){
+    int i=0, e=0, d=0;
+
+    /*escrevendo de volta no vetor v*/
+    while((e < tam_esq) && (d < tam_dir)){
+        if(comparar_pratos(esquerda[e], direita[d])){
+            v[i] = esquerda[e];
+            e++;
+        }
+        else{
+            v[i] = direita[d];
+            d++;
+        }
+        i++;
+    }
+
+    /*caso não sobre elementos em um vetor para compararmos*/
+    while(e < tam_esq){
+        v[i] = esquerda[e];
+        e++;
+        i++;
+    }
+    while(d < tam_dir){
+        v[i] = direita[d];
+        d++;
+        i++;
+    }
+
+    return;
+}
+
+void mergesort(PRATO *v, int tam){
+    if(tam == 1){ //caso base
+        return;
+    }
+
+    int meio = tam/2;
+    PRATO esquerda[meio], direita[tam-meio];
+
+    /*dividindo o vetor v[] em dois (esquerda[] e direita[]), e passando os valores pra cada um*/
+    int e=0, d=0;
+    for(int i=0; i<tam; i++){
+        if(i < meio){
+            esquerda[e] = v[i];
+            e++;
+        }
+        else{
+            direita[d] = v[i];
+            d++;
+        }
+    }
+
+    mergesort(esquerda, meio); //note que v[] é o vetor esquerda[]!
+    mergesort(direita, tam-meio);
+    intercala(esquerda, meio, direita, tam-meio, v);
+
+    return;
 }
 
 //Retorna true se o prato1 deve ter maior prioridade que o prato2
