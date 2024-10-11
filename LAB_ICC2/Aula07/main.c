86d8b16a858da41459aa83db79e7074cf54d2e07
Alec Aoki <alec.camposaoki@gmail.com>
2024-10-11 14:56:52 -0300

    ler_cartas working?

--- LAB_ICC2/Aula07/main.c
+++ LAB_ICC2/Aula07/main.c
@@ -3,10 +3,30 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef struct carta_{
+    char naipe[3];
+    char *valores;
+} CARTA;
+
+void ler_cartas(CARTA baralho[], int quantCartas, int quantValores);
+
 int main(void){
-    char teste[3];
-    scanf("%s", teste);
-    printf("%s\n", teste);
+    int quantCartas, quantValores;
+    scanf("%d %d", &quantCartas, &quantValores);
+
+    CARTA baralho[quantCartas];
+    ler_cartas(baralho, quantCartas, quantValores);
+
+    for(int i=0; i<quantCartas; i++){
+        printf("%s %s\n", baralho[i].naipe, baralho[i].valores);
+    }
     
     return 0;
+}
+
+void ler_cartas(CARTA baralho[], int quantCartas, int quantValores){
+    for(int i=0; i<quantCartas; i++){
+        baralho[i].valores = (char *)malloc((quantValores+1)*sizeof(char));
+        scanf(" %s %s", baralho[i].naipe, baralho[i].valores);
+    }
 }
