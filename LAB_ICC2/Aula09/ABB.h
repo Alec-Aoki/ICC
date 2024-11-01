0905ddb6701bada5b93cf5f5bf6bdcd9475293d5
Alec Aoki <alec.camposaoki@gmail.com>
2024-11-01 15:02:36 -0300

    Add ABB declarations

---
+++ LAB_ICC2/Aula09/ABB.h
@@ -0,0 +1,13 @@
+#ifndef ABB_H
+  #define ABB_H
+
+  typedef struct abb_ ABB;
+
+  ABB *abb_criar(void);
+  bool abb_apagar(ABB **arvore);
+  ABB *abb_inserir(ABB *arvore, int elemento);
+  int abb_remover(ABB *arvore, int elemento);
+  void abb_imprimir(ABB *arvore);
+  bool abb_busca(ABB *arvore, int elemento);
+
+#endif
